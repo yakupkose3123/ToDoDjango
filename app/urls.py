58fbc53af6369784dff8d42d93a5278059abcff1
yakupkose3123 <yakupkose3123@gmail.com>
2022-06-11 19:05:13 +0300

    end of proj with class based and function based views

--- app/urls.py
+++ app/urls.py
@@ -1,18 +1,20 @@
 
 from django.urls import path
-from django.views.generic.base import TemplateView
-from .views import  HomeView, TodoListView , TodoCreateView , TodoDetailView , TodoUpdateView , TodoDeleteView
+from .views import  home, todo_delete, todo_list, todo_create,  HomeView, TodoListView , TodoCreateView , TodoDetailView , TodoUpdateView , TodoDeleteView,  todo_update
 
 urlpatterns = [
-   path('', HomeView.as_view(),  name="home"),
+   #! CLASS BASED VIEWS
+   # path('', HomeView.as_view(),  name="home"),
+   # path('list/', TodoListView.as_view(), name="list"),
+   # path('add/', TodoCreateView.as_view(), name="add"),
+   # path('update/<int:pk>/', TodoUpdateView.as_view(), name="update"),
+   # path('delete/<int:pk>/', TodoDeleteView.as_view(), name="delete"),
 
-   path('list/', TodoListView.as_view(), name="list"),
-
-   path('add/', TodoCreateView.as_view(), name="add"), 
-
-   path('detail/<int:id>/', TodoDetailView.as_view(), name="detail"), 
-
-   path('update/<int:pk>/', TodoUpdateView.as_view(), name="update"),
-
-   path('delete/<int:pk>/', TodoDeleteView.as_view(), name="delete"),
+   #! FUNTİONAL BASED VIEWS
+   path('', home,  name="home"),
+   path('list/', todo_list,  name="list"),   
+   path('add/', todo_create, name="add"), 
+   path('detail/<int:id>/', TodoDetailView.as_view(), name="detail"),  
+   path('update/<int:id>/', todo_update, name="update"),   
+   path('delete/<int:id>/', todo_delete, name="delete"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,24 +1,47 @@
+from multiprocessing import context
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import ToDoForm
+from .models import Todo
 
 from app.models import Todo
 
-
+#!HOME
+def home(request) :
+    return render(request, "app/home.html")
 class HomeView(TemplateView):
     template_name = "app/home.html" 
 
 #!LİST
+def todo_list(request) : 
+    todos = Todo.objects.all()
+    context = {"todos" : todos}
+
+    return render(request, "app/todo_list.html", context)
+
+
 class TodoListView(ListView):
     model = Todo
     context_object_name = 'todos'
 
 #!ADD
+
+def todo_create(request):
+    form = ToDoForm()
+
+    if request.method == "POST":
+        form = ToDoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context = { "form" : form}
+    return render(request, "app/todo_add.html", context)
 
 class TodoCreateView(CreateView):
     model = Todo
@@ -32,7 +55,21 @@ class TodoDetailView(DetailView):
     pk_url_kwarg = 'id'
 
 #!UPDATE
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)    
+    form = ToDoForm(instance=todo)
 
+    if request.method == "POST":
+        form = ToDoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+    context = { 
+        "todo" : todo,
+        "form" : form
+        }
+
+    return render(request, "app/todo_update.html", context)
 class TodoUpdateView(UpdateView):
     model = Todo
     form_class = ToDoForm 
@@ -40,6 +77,16 @@ class TodoUpdateView(UpdateView):
     success_url = reverse_lazy('list')
 
 #! DELETE 
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+
+    if request.method == "POST":
+        todo.delete()
+        return redirect("list") 
+    context = {"todo" : todo}   
+
+    return render(request, "app/todo_delete.html", context)
 
 class TodoDeleteView(DeleteView):
     model = Todo
